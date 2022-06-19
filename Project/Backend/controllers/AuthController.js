9ac7a333ecb9ceda3de93553c0c785d3bf31f24d
phammanhhung <phammanhhung10501@gmail.com>
2022-06-19 20:18:16 +0700

    add feature register and login

--- Project/Backend/controllers/AuthController.js
+++ Project/Backend/controllers/AuthController.js
@@ -1,6 +1,8 @@
-const Auth = require('../models/Auth');
+const Customer = require('../models/Customer');
 const BaseController = require('../framework').BaseController;
 const CustomError = require('../framework').CustomError;
+const { hashPassword, comparePassword } = require('../utils/password.utils');
+const jwt = require('jsonwebtoken');
 
 class AuthController extends BaseController {
     async register() {
@@ -34,12 +36,59 @@ class AuthController extends BaseController {
             throw new CustomError.BadRequestError("Date of birth is invalid");
         }
 
+        const registeredCustomer = await Customer.findByEmail(email);
+        if (registeredCustomer && registeredCustomer.length > 0) {
+            throw new CustomError.BadRequestError("Email is already registered");
+        }
+
+        const {
+            hashedPassword,
+            salt
+        } = await hashPassword(password);
+
         try {
-            const customer = await Auth.register(username, email, password, phone, gender, dob);
-            return this.ok(customer);
+            const customer = await Customer.create(username, email, hashedPassword, phone, gender, dob, salt);
+            return this.ok({
+                success: true,
+                id: customer[0]["insertId"]
+            });
         } catch (err) {
             throw err;
         }
+    }
+
+    async login() {
+        const { email, password } = this.body;
+
+        if (!email || !password) {
+            throw new CustomError.BadRequestError("Not enough information provided");
+        }
+        
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+            throw new CustomError.BadRequestError("Invalid email");
+        }
+
+        const customers = await Customer.findByEmail(email);
+        if (!customers || customers.length === 0) {
+            throw new CustomError.BadRequestError("Invalid credentials");
+        }
+
+        const customer = customers[0];
+        if (!(await comparePassword(password, customer["PASSWORD"]))) {
+            throw new CustomError.BadRequestError("Invalid credentials");
+        }
+
+        const token = jwt.sign({
+            id: customer["id"],
+            name: customer["username"],
+        }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_LIFETIME
+        });
+
+        return this.ok({
+            success: true,
+            accessToken: token
+        });
     }
 }
 
--- Project/Backend/models/Customer.js
+++ Project/Backend/models/Customer.js
@@ -1,7 +1,24 @@
 const pool = require('../config/db.config');
 
 class Customer {
-    
+    async findByEmail(email) {
+        if (!email) {
+            return null;
+        }
+        const [rows] = await pool.query("SELECT * FROM CUSTOMER WHERE EMAIL = ?", email);
+        return rows;
+    }
+
+    async create(username, email, password, phone, gender, dob, salt) {
+        if (dob) {
+            const dateofbirth = new Date(dob);
+            dob = dateofbirth.getDate() + '/' + (dateofbirth.getMonth() + 1) + '/' + dateofbirth.getFullYear();
+        }
+        const customer = 
+            await pool.query("INSERT INTO CUSTOMER(name, email, password, phone, gender, dob, salt) VALUES(?, ?, ?, ?, ?, ?, ?)",
+                            [username, email, password, phone || 'null', gender, dob || 'null', salt]);
+        return customer;
+    }
 }
 
 module.exports = new Customer();
