c43fbae29b14a9bf5253982a99b28cb008c6c620
phammanhhung <phammanhhung10501@gmail.com>
2022-06-25 20:50:48 +0700

    add order route

--- Project/Backend/routes/order.route.js
+++ Project/Backend/routes/order.route.js
@@ -2,3 +2,5 @@ const Router = require('../framework').Router;
 const AuthMiddleware = require('../middlewares/authentication');
 
 Router.get('/order', 'OrderController#getAllOrders', [AuthMiddleware]);
+Router.get('/order/:orderId', 'OrderController#getOrder', [AuthMiddleware]);
+Router.post('/order/:orderId/updatestatus', 'OrderController#updateStatus', [AuthMiddleware]);
