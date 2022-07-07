680dc0b500c98da686594a64ae79c191bc0a2838
phammanhhung <phammanhhung10501@gmail.com>
2022-07-07 15:38:13 +0700

    Refactor publisher model + controller

--- Project/Backend/controllers/PublisherController.js
+++ Project/Backend/controllers/PublisherController.js
@@ -7,7 +7,7 @@ class PublisherController extends BaseController {
         let page = Number(this.query.page) || 1;
         const limit = Number(this.query.limit) || 10;
 
-        const publishers = await Publisher.getAll();
+        const publishers = await Publisher.where().all();
 
         const count = publishers.length;
         const totalPage = Math.floor(count / limit) + ((count % limit == 0) ? 0 : 1);
@@ -25,66 +25,63 @@ class PublisherController extends BaseController {
 
     async get() {
         const publisherId = this.params.publisherId;
-        if (!publisherId ||publisherId < 0) {
-            throw new CustomError.BadRequestError("Publisher id is invalid");
+        if (!publisherId) {
+            throw new CustomError.BadRequestError("Publisher id is missing");
         }
 
         const publisher = await Publisher.findById(publisherId);
         if (publisher == null) {
-            throw new CustomError.NotFoundError("publisher not found");
+            throw new CustomError.NotFoundError("Publisher not found");
         }
 
         return this.ok(publisher);
     }
 
     async create() {
-        const publisherName = this.body.name;
-        if (!publisherName) {
-            throw new CustomError.BadRequestError("publisher's name is missing");
+        const result = await Publisher.create({ name: this.body.name });
+        if (result.success) {
+            return this.created({ insertedId: result.insertedId });
+        } else {
+            throw new CustomError.BadRequestError(result.errors);
         }
-
-        const insertedId = await Publisher.create(publisherName);
-        if (insertedId) {
-            return this.created({ insertedId });
-        }
-
-        throw new Error("Something went wrong! Please try again!");
     }
 
     async update() {
         const publisherName = this.body.name;
         const publisherId = this.body.publisherId;
 
-        if (!publisherId || !publisherName) {
-            throw new CustomError.BadRequestError("publisher's name or id is missing");
+        if (!publisherId) {
+            throw new CustomError.BadRequestError("publisher's id is missing");
         }
 
-        if ((await Publisher.findById(publisherId)) == null) {
-            throw new CustomError.BadRequestError("publisher not found");
-        }
-
-        if (await Publisher.update(publisherId, publisherName)) {
+        if (!publisherName) {
             return this.noContent();
         }
 
-        throw new Error("Something went wrong! Please try again!");
+        const result = await Publisher.update({ id: publisherId }, { name: publisherName });
+        if (result.success) {
+            return this.noContent();
+        } else if (typeof (result.errors) === "string") {
+            throw new CustomError.NotFoundError("Publisher not found");
+        } else {
+            throw new CustomError.BadRequestError(result.errors);
+        }
     }
 
     async delete() {
         const publisherId = this.params.publisherId;
         if (!publisherId) {
-            return new CustomError.BadRequestError("publisher's name is missing");
+            return new CustomError.BadRequestError("Publisher's id is missing");
         }
 
-        if ((await Publisher.findById(publisherId)) == null) {
-            throw new CustomError.BadRequestError("publisher not found");
-        }
-
-        if (await Publisher.delete(publisherId)) {
+        const result = await Publisher.delete({ id: publisherId });
+        if (result.success) {
             return this.noContent();
+        } else if (typeof (result.errors) === "string") {
+            throw new CustomError.NotFoundError("Publisher not found");
+        } else {
+            throw new CustomError.BadRequestError(result.errors);
         }
-
-        throw new Error("Something went wrong! Please try again!");
     }
 }
 
--- Project/Backend/models/Publisher.js
+++ Project/Backend/models/Publisher.js
@@ -1,63 +1,11 @@
-const pool = require('../config/db.config');
-const CustomError = require('../framework').CustomError;
+const BaseModel = require('../framework').BaseModel;
+const { AttributeType, Validators } = require('../framework/ModelHelpers');
 
-class Publisher {
-    async getAll() {
-        const [rows] = await pool.query("SELECT * FROM publisher");
-        return rows;
-    }
-
-    async findById(id) {
-        if (!id || id < 0) {
-            return null;
-        }
-
-        const [rows] = await pool.query("SELECT * FROM publisher WHERE id = ?", [id]);
-        //console.log(rows);
-        if (rows.length === 0)
-            return null;
-        return rows[0];
-    }
-
-    async create(name) {
-        if (!name) {
-            return new CustomError.BadRequestError("Name is required");
-        }
-
-        const [rows] = await pool.query("INSERT INTO publisher(name) VALUES(?)", [name]);
-        //console.log(rows);
-        if (rows.affectedRows === 1) {
-            return rows.insertId;
-        }
-
-        return false;
-    }
-
-    async update(id, name) {
-        if (!id || !name) {
-            throw new CustomError.BadRequestError("Not enough information provided");
-        }
-
-        const [rows] = await pool.query("UPDATE publisher SET name = ? WHERE id = ?", [name, id]);
-        if (rows.affectedRows === 1) {
-            return true;
-        }
-
-        return false;
-    }
-
-    async delete(id) {
-        if (!id) {
-            throw new CustomError.BadRequestError("Not enough information provided");
-        }
-
-        const [rows] = await pool.query("DELETE FROM publisher WHERE id = ?", [id]);
-        if (rows.affectedRows === 1) {
-            return true;
-        }
-
-        return false;
+class Publisher extends BaseModel {
+    setup() {
+        this.setTablename('publisher');
+        this.setAttribute('name', AttributeType.String, [Validators.Required, Validators.MaxLength(255), Validators.MinLength(1)]);
     }
 }
 
-module.exports = new Publisher();
+module.exports = Publisher;
