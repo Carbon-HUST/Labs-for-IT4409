92c2c8cea4b6688eaed8156ed62c5bab47e36b56
phammanhhung <phammanhhung10501@gmail.com>
2022-07-07 15:16:19 +0700

    Remove unnecessary require in Genre model

--- Project/Backend/models/Genre.js
+++ Project/Backend/models/Genre.js
@@ -1,15 +1,9 @@
 const BaseModel = require('../framework').BaseModel;
 const { AttributeType, Validators } = require('../framework/ModelHelpers');
-const pool = require('../config/db.config');
-
 class Genre extends BaseModel {
     setup() {
         this.setTablename('genre');
         this.setAttribute('name', AttributeType.String, [Validators.Required, Validators.MaxLength(255), Validators.MinLength(1)]);
-    }
-    async getAll() {
-        const [rows] = await pool.query("SELECT * FROM genre");
-        return rows;
     }
 }
 
