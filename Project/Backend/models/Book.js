e5922e984f0a0afde01b58063478ff03ba731649
AnhNH01 <haianh552@gmail.com>
2022-07-09 00:28:19 +0700

    Delete redundant code in Book model

--- Project/Backend/models/Book.js
+++ Project/Backend/models/Book.js
@@ -16,60 +16,6 @@ class Book extends BaseModel {
         this.setAttribute('description', AttributeType.String, [Validators.MaxLength(255)]);
     }
 
-
-    // async findById(id) {
-    //     if(!id)
-    //         return null;
-    //     const [rows] = await pool.query("SELECT * FROM book WHERE ID = ?", [id]);
-
-    //     const [authorRows] = await pool.query("SELECT author.NAME " + 
-    //                                           "FROM author, book_author " +
-    //                                           "WHERE book_author.AUTHOR_ID = author.ID AND book_author.BOOK_ID = ?", [id]);
-        
-    //     const [genreRows] = await pool.query("SELECT genre.NAME " +
-    //                                          "FROM genre, book_genre " +
-    //                                          "WHERE book_genre.GENRE_ID = genre.ID AND book_genre.BOOK_ID = ?", [id]);
-
-    //     if(rows.length === 0)
-    //         return null;
-
-    //     let row = rows[0];
-    //     row.authors = authorRows;
-    //     row.genres = genreRows;
-
-    //     return rows;
-    // }
-
-    // // Filter by Genre
-    // async filterByGenre(id, page, limit) {
-    //     if(!id)
-    //         return null;
-        
-    //     const skip = (page - 1) * limit;
-
-    //     const [rows] = await pool.query('SELECT book.* FROM book, book_genre ' + 
-    //                                     'WHERE book.ID = book_genre.BOOK_ID ' +
-    //                                     'AND book_genre.GENRE_ID = ? ' +
-    //                                     'LIMIT ? OFFSET ?', [id, parseInt(limit), parseInt(skip)]);
-        
-    //     return rows;
-    // }
-
-
-    // // Filter by author
-    // async filterByAuthor(id, page, limit) {
-    //     if(!id)
-    //         return null;
-        
-    //     const skip = (page - 1) * limit;
-
-    //     const [rows] = await pool.query('SELECT book.* FROM book, book_author ' + 
-    //                                     'WHERE book.ID = book_author.BOOK_ID ' +
-    //                                     'AND book_author.AUTHOR_ID = ? ' +
-    //                                     'LIMIT ? OFFSET ?', [id, parseInt(limit), parseInt(skip)]);
-        
-    //     return rows;
-    // }
 }
 
 module.exports = Book;
