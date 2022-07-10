ac7c68afe8ac7e14b48efc67e93288e7a875de41
phammanhhung <phammanhhung10501@gmail.com>
2022-07-10 15:40:54 +0700

    Fix cors

--- Project/Backend/server.js
+++ Project/Backend/server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 require('./config/cloudinary.config').config();
-
+const cors = require('cors');
 const Carbon = require('./framework');
 
+const corsOptions = {
+    origin: process.env.ALLOWED_ORIGIN.split(' '),
+    optionsSuccessStatus: 200
+}
+
 const app = Carbon();
+
+app.useMiddleware(cors(corsOptions));
 
 app.listen(3000);
