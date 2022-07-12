1c46d7bd8baafa293d09fe9885f431f3a3f7667d
AnhNH01 <66894681+AnhNH01@users.noreply.github.com>
2022-07-12 21:32:41 +0700

    Revert "Feature/book"

--- Project/Backend/routes/admin.route.js
+++
@@ -1,6 +0,0 @@
-const authmiddleware = require('../middlewares/adminAuthentication')
-
-const Router = require('../framework').Router;
-
-Router.post('/admin/auth/login', 'AdminController#login');
-Router.put('/admin/auth/changepassword', 'AdminController#changePassword', [authmiddleware]);
--- Project/Backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-require('./config/cloudinary.config').config();
-
-const Carbon = require('./framework');
-
-const app = Carbon();
-
-app.listen(3000);
