a4ae14ee97211318acca905e93417f6c9aec5d16
Phạm Mạnh Hùng <72314586+PhamHung2020@users.noreply.github.com>
2022-07-21 11:25:08 +0700

    Revert "Backend"

--- Project/Backend/routes/auth.route.js
+++
@@ -1,4 +0,0 @@
-const Router = require('../framework').Router;
-
-Router.post('/auth/register', 'AuthController#register');
-Router.post('/auth/login', 'AuthController#login');
--- Project/Backend/routes/author.route.js
+++
@@ -1,8 +0,0 @@
-const Router = require('../framework').Router;
-const AdminAuthenticationMiddleware = require('../middlewares/adminAuthentication');
-
-Router.get('/admin/author', 'AuthorController#getAll');
-Router.get('/admin/author/:authorId', 'AuthorController#get');
-Router.post('/admin/author', 'AuthorController#create', [AdminAuthenticationMiddleware]);
-Router.put('/admin/author', 'AuthorController#update', AdminAuthenticationMiddleware);
-Router.delete('/admin/author/:authorId', 'AuthorController#delete', AdminAuthenticationMiddleware);
